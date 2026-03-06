62ed59def0bcdeff07820cb29988f44cea02f073
SUJEET662 <mainsujeethoon@gmail.com>
2026-03-06 12:36:52 +0530

    backend added

---
+++ backened/index.js
@@ -0,0 +1,46 @@
+import express from 'express';
+import cors from 'cors';
+import morgan from "morgan"
+
+const app = express();
+const PORT = 5000;
+
+app.use(express.json());
+app.use(cors());
+app.use(morgan('dev'))
+
+app.get('/', (req, res) => {
+  res.send("Backend is running");
+});
+
+app.post('/login', (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  console.log(email,password);
+
+  
+  if (!email || !password) {
+    return res.status(400).json({
+      success: false,
+      message: "Email and password required"
+    });
+  }
+
+  
+  if (email === "sy960004@gmail.com" && password === "1234") {
+    return res.status(200).json({
+      success: true,
+      message: "Login successful"
+    });
+  }
+
+
+  return res.status(401).json({
+    success: false,
+    message: "Invalid credentials"
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
